63ac6cda3b0951fff57feacb2d98d4df80ddfc7d
peth pimartharn <padtaraya.g@gmail.com>
2024-04-19 00:41:48 +0700

    create function

--- javascript/countingSort.js
+++ javascript/countingSort.js
@@ -1,3 +1,22 @@
 function countingSort(arr){
-    let max = Math.max(...arr)
+    let max_val = Math.max(...arr)
+    let count = Array(max_val + 1).fill(0)
+    
+    while (arr.length > 0) {
+        let num = arr.shift()
+        count[num] +=  1
+    }
+
+    let sortedArr  = []
+    for(let i = 0; i < count.lenght; i++){
+        while (count[i] > 0){
+            sortArray.push(i)
+            count[i] -= 1
+        }
+    }
+    return sortedArr 
 }
+
+let unsortedArr = [4, 2, 2, 6, 3, 3, 1, 6, 5, 2, 3]
+let sortedArr  = countingSort(unsortedArr)
+console.log("Sorted array:", sortedArr )
