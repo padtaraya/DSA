3815aa07fbf3e48f51253d1552aba5c01c72c4ae
unknown <padtaraya.g@gmail.com>
2024-04-21 23:09:17 +0700

    selection sort with python

---
+++ python/selectionSort.py
@@ -0,0 +1,19 @@
+my_array = [64, 34, 55, 22, 12, 45, 99]
+
+n = len(my_array)
+
+for i in range(n - 1):
+
+    min_index = 1
+
+    for j in range(i + 1, n): 
+
+        if my_array[j] < my_array[min_index]:
+
+            min_index = j
+
+    min_value = my_array.pop(min_index)
+
+    my_array.insert(i,min_value)
+
+print("Sort array: ", my_array)
