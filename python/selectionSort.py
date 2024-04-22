0da680d4c741a0ab473494436c686006a6ba98bb
padtaraya <padtaraya.g@gmail.com>
2024-04-22 11:51:51 +0700

    add

--- python/selectionSort.py
+++ python/selectionSort.py
@@ -5,15 +5,19 @@ n = len(my_array)
 for i in range(n - 1):
 
     min_index = 1
-
+    print("i = ", i)
     for j in range(i + 1, n): 
-
+        # print("j = ", j)
+        # print("my_array[j] = ", my_array[j])
         if my_array[j] < my_array[min_index]:
 
             min_index = j
 
     min_value = my_array.pop(min_index)
-
+    print("min_value ", min_value)
     my_array.insert(i,min_value)
+    print(my_array)
+
+    
 
 print("Sort array: ", my_array)
