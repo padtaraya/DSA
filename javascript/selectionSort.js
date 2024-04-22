9fd4ddc19ed3077b38be864b07bf61b44abe4737
padtaraya <padtaraya.g@gmail.com>
2024-04-22 23:51:32 +0700

    selection sort js

--- javascript/selectionSort.js
+++ javascript/selectionSort.js
@@ -3,18 +3,18 @@ let my_array = [64, 34, 55, 22, 12, 45, 99]
 let n = my_array.length
 
 for(i = 0; i < n - 1; i ++){
-    console.log(i);
+
     let min_index = 1
-    console.log("min ",min_index);
+
     for(j = i + 1; j <  n; j++){
-        console.log("j : ", j);
+
         if(my_array[j] < my_array[min_index]){
-            console.log("in if");
+
             min_index = j
         }
     }
     let min_value = my_array.splice(min_index,1)
-    console.log("min value ", Number(min_value[0]));
+
     my_array.splice(i, 0,Number(min_value[0]))
    
 }
