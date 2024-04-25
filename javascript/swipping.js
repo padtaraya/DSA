fee63745fa39993532a1123b4e33e6f7a8abfa16
padtaraya <padtaraya.g@gmail.com>
2024-04-25 23:56:34 +0700

    edit swipping

--- javascript/swipping.js
+++ javascript/swipping.js
@@ -7,18 +7,21 @@ for (let i = 0; i < len; i++) {
     
     let min_index = i
 
+    
     for (let j = i + 1; j < len; j++) {
         if (my_array[j] < my_array[min_index]) {
             min_index = j
         }
 
     }
+
     let chunk = my_array[i]
     my_array[i] = my_array[min_index]
     my_array[min_index] = chunk
 
 
 }
+
 
 console.log(my_array);
 
